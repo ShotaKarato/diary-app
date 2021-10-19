e56ff4fdbba0a629ed6112ba0d5a616d05d8e99a
ShotaKarato <theseuta@gmail.com>
2021-10-19 15:28:40 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { auth } from "./firebase";
 
 function App() {
+  console.log(auth);
   return (
     <div className="App">
       <header className="App-header">
